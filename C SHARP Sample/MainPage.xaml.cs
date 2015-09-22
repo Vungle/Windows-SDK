4c54094bd1efb6985d8f1f72f3bb6057198d667f
Vadim Zasovin <vzasovin@gmail.com>
2015-09-22 19:24:44 +0200

    c# sample app

---
+++ C SHARP Sample/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using VungleSDK;
+using System;
+
+namespace C_SHARP_Sample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        VungleAd sdkInstance;
+
+        public MainPage()
+        {
+            sdkInstance = AdFactory.GetInstance("pushokTest");
+            sdkInstance.OnAdPlayableChanged += SdkInstance_OnAdPlayableChanged;
+            this.InitializeComponent();
+        }
+
+        private void SdkInstance_OnAdPlayableChanged(object sender, AdPlayableEventArgs e)
+        {
+            if (e.AdPlayable)
+            {
+                //ad became playable
+            }
+            else
+            {
+                //ad became not playable
+            }
+        }
+
+        private async void DefaultConfigButton_Click(object sender, RoutedEventArgs e)
+        {
+            await sdkInstance.PlayAdAsync(new AdConfig());
+        }
+
+        private async void MutedConfigButton_Click(object sender, RoutedEventArgs e)
+        {
+            await sdkInstance.PlayAdAsync(new AdConfig { SoundEnabled = false });
+        }
+
+        private async void IncentivizedConfigButton_Click(object sender, RoutedEventArgs e)
+        {
+            await sdkInstance.PlayAdAsync(new AdConfig { Incentivized = true });
+        }
+    }
+}
