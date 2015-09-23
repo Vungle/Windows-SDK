eaadc605cb0f9d5c992dd553c2fe6d6289f40497
Vadim Zasovin <vzasovin@gmail.com>
2015-09-23 12:34:54 +0200

    visual basic sample app

---
+++ VB_sample/MainPage.xaml.vb
@@ -0,0 +1,31 @@
+﻿' The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+Imports VungleSDK
+
+Public NotInheritable Class MainPage
+    Inherits Page
+
+    Private sdkInstance As VungleAd
+
+    Public Sub New()
+        InitializeComponent()
+        sdkInstance = AdFactory.GetInstance("pushokTest")
+    End Sub
+
+    Private Async Sub DefaultConfigButton_Click(sender As Object, e As RoutedEventArgs)
+        Dim adConfig As New AdConfig
+        Await sdkInstance.PlayAdAsync(adConfig)
+    End Sub
+
+    Private Async Sub IncentivizedConfigButton_Click(sender As Object, e As RoutedEventArgs)
+        Dim adConfig As New AdConfig
+        adConfig.Incentivized = True
+        Await sdkInstance.PlayAdAsync(adConfig)
+    End Sub
+
+    Private Async Sub MutedConfigButton_Click(sender As Object, e As RoutedEventArgs)
+        Dim adConfig As New AdConfig
+        adConfig.SoundEnabled = False
+        Await sdkInstance.PlayAdAsync(adConfig)
+    End Sub
+End Class
