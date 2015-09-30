45ad6f0e00f2852fa1685e7a64ba88b72e4c2f59
vadimZasovin <vzasovin@gmail.com>
2015-09-30 19:21:36 +0300

    added DirectX+XAML sample app

---
+++ DirectX_XAML_sample/DirectXPage.xaml.cpp
@@ -0,0 +1,223 @@
+﻿//
+// DirectXPage.xaml.cpp
+// Implementation of the DirectXPage class.
+//
+
+#include "pch.h"
+#include "DirectXPage.xaml.h"
+
+using namespace DirectX_XAML_sample;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::Graphics::Display;
+using namespace Windows::System::Threading;
+using namespace Windows::UI::Core;
+using namespace Windows::UI::Input;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+using namespace concurrency;
+using namespace Windows::ApplicationModel::Core;
+//Using VungleSDK namespace
+using namespace VungleSDK;
+
+DirectXPage::DirectXPage():
+	m_windowVisible(true),
+	m_coreInput(nullptr)
+{
+	InitializeComponent();
+
+	//Obtain Vungle SDK instance
+	sdkInstance = AdFactory::GetInstance("vungleTest");
+
+	//Register OnAdPlayableChanged event handler
+	sdkInstance->OnAdPlayableChanged += ref new Windows::Foundation::
+		EventHandler<VungleSDK::AdPlayableEventArgs ^>
+		(this, &DirectX_XAML_sample::DirectXPage::OnOnAdPlayableChanged);
+
+	//Rest initialization
+	CoreWindow^ window = Window::Current->CoreWindow;
+
+	window->VisibilityChanged +=
+		ref new TypedEventHandler<CoreWindow^, VisibilityChangedEventArgs^>(this, &DirectXPage::OnVisibilityChanged);
+
+	DisplayInformation^ currentDisplayInformation = DisplayInformation::GetForCurrentView();
+
+	currentDisplayInformation->DpiChanged +=
+		ref new TypedEventHandler<DisplayInformation^, Object^>(this, &DirectXPage::OnDpiChanged);
+
+	currentDisplayInformation->OrientationChanged +=
+		ref new TypedEventHandler<DisplayInformation^, Object^>(this, &DirectXPage::OnOrientationChanged);
+
+	DisplayInformation::DisplayContentsInvalidated +=
+		ref new TypedEventHandler<DisplayInformation^, Object^>(this, &DirectXPage::OnDisplayContentsInvalidated);
+
+	swapChainPanel->CompositionScaleChanged += 
+		ref new TypedEventHandler<SwapChainPanel^, Object^>(this, &DirectXPage::OnCompositionScaleChanged);
+
+	swapChainPanel->SizeChanged +=
+		ref new SizeChangedEventHandler(this, &DirectXPage::OnSwapChainPanelSizeChanged);
+
+	m_deviceResources = std::make_shared<DX::DeviceResources>();
+	m_deviceResources->SetSwapChainPanel(swapChainPanel);
+
+	auto workItemHandler = ref new WorkItemHandler([this] (IAsyncAction ^)
+	{
+		m_coreInput = swapChainPanel->CreateCoreIndependentInputSource(
+			Windows::UI::Core::CoreInputDeviceTypes::Mouse |
+			Windows::UI::Core::CoreInputDeviceTypes::Touch |
+			Windows::UI::Core::CoreInputDeviceTypes::Pen
+			);
+
+		m_coreInput->PointerPressed += ref new TypedEventHandler<Object^, PointerEventArgs^>(this, &DirectXPage::OnPointerPressed);
+		m_coreInput->PointerMoved += ref new TypedEventHandler<Object^, PointerEventArgs^>(this, &DirectXPage::OnPointerMoved);
+		m_coreInput->PointerReleased += ref new TypedEventHandler<Object^, PointerEventArgs^>(this, &DirectXPage::OnPointerReleased);
+
+		m_coreInput->Dispatcher->ProcessEvents(CoreProcessEventsOption::ProcessUntilQuit);
+	});
+
+	m_inputLoopWorker = ThreadPool::RunAsync(workItemHandler, WorkItemPriority::High, WorkItemOptions::TimeSliced);
+
+	m_main = std::unique_ptr<DirectX_XAML_sampleMain>(new DirectX_XAML_sampleMain(m_deviceResources));
+	m_main->StartRenderLoop();
+}
+
+
+void DirectX_XAML_sample::DirectXPage::DefaultConfigButton_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	//Play ad with default configuration
+	sdkInstance->PlayAdAsync(ref new AdConfig());
+}
+
+
+void DirectX_XAML_sample::DirectXPage::IncentivizedConfigButton_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	//Play ad with enabled 'incentivized' option
+	AdConfig^ adConfig = ref new AdConfig();
+	adConfig->Incentivized = true;
+	sdkInstance->PlayAdAsync(adConfig);
+}
+
+
+void DirectX_XAML_sample::DirectXPage::MutedConfigButton_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	//Play ad without sound
+	AdConfig^ adConfig = ref new AdConfig();
+	adConfig->SoundEnabled = false;
+	sdkInstance->PlayAdAsync(adConfig);
+}
+
+//Event handler for OnAdPlayableChanged event 
+void DirectX_XAML_sample::DirectXPage::OnOnAdPlayableChanged(Platform::Object ^sender, VungleSDK::AdPlayableEventArgs ^args)
+{
+	//Run asynchronously on the UI thread
+	CoreApplication::MainView->Dispatcher->RunAsync(Windows::UI::Core::CoreDispatcherPriority::Normal,
+		ref new Windows::UI::Core::DispatchedHandler(
+			[this, args]
+	{
+		//Change IsEnabled property for each button
+		bool adPlayable = args->AdPlayable;
+		this->DefaultConfigButton->IsEnabled = adPlayable;
+		this->IncentivizedConfigButton->IsEnabled = adPlayable;
+		this->MutedConfigButton->IsEnabled = adPlayable;
+	}));
+}
+
+DirectXPage::~DirectXPage()
+{
+	m_main->StopRenderLoop();
+	m_coreInput->Dispatcher->StopProcessEvents();
+}
+
+void DirectXPage::SaveInternalState(IPropertySet^ state)
+{
+	critical_section::scoped_lock lock(m_main->GetCriticalSection());
+	m_deviceResources->Trim();
+
+	m_main->StopRenderLoop();
+
+}
+
+void DirectXPage::LoadInternalState(IPropertySet^ state)
+{
+	m_main->StartRenderLoop();
+}
+
+
+void DirectXPage::OnVisibilityChanged(CoreWindow^ sender, VisibilityChangedEventArgs^ args)
+{
+	m_windowVisible = args->Visible;
+	if (m_windowVisible)
+	{
+		m_main->StartRenderLoop();
+	}
+	else
+	{
+		m_main->StopRenderLoop();
+	}
+}
+
+
+void DirectXPage::OnDpiChanged(DisplayInformation^ sender, Object^ args)
+{
+	critical_section::scoped_lock lock(m_main->GetCriticalSection());
+	m_deviceResources->SetDpi(sender->LogicalDpi);
+	m_main->CreateWindowSizeDependentResources();
+}
+
+void DirectXPage::OnOrientationChanged(DisplayInformation^ sender, Object^ args)
+{
+	critical_section::scoped_lock lock(m_main->GetCriticalSection());
+	m_deviceResources->SetCurrentOrientation(sender->CurrentOrientation);
+	m_main->CreateWindowSizeDependentResources();
+}
+
+
+void DirectXPage::OnDisplayContentsInvalidated(DisplayInformation^ sender, Object^ args)
+{
+	critical_section::scoped_lock lock(m_main->GetCriticalSection());
+	m_deviceResources->ValidateDevice();
+}
+
+void DirectXPage::AppBarButton_Click(Object^ sender, RoutedEventArgs^ e)
+{
+}
+
+void DirectXPage::OnPointerPressed(Object^ sender, PointerEventArgs^ e)
+{
+	m_main->StartTracking();
+}
+
+void DirectXPage::OnPointerMoved(Object^ sender, PointerEventArgs^ e)
+{
+	if (m_main->IsTracking())
+	{
+		m_main->TrackingUpdate(e->CurrentPoint->Position.X);
+	}
+}
+
+void DirectXPage::OnPointerReleased(Object^ sender, PointerEventArgs^ e)
+{
+	m_main->StopTracking();
+}
+
+void DirectXPage::OnCompositionScaleChanged(SwapChainPanel^ sender, Object^ args)
+{
+	critical_section::scoped_lock lock(m_main->GetCriticalSection());
+	m_deviceResources->SetCompositionScale(sender->CompositionScaleX, sender->CompositionScaleY);
+	m_main->CreateWindowSizeDependentResources();
+}
+
+void DirectXPage::OnSwapChainPanelSizeChanged(Object^ sender, SizeChangedEventArgs^ e)
+{
+	critical_section::scoped_lock lock(m_main->GetCriticalSection());
+	m_deviceResources->SetLogicalSize(e->NewSize);
+	m_main->CreateWindowSizeDependentResources();
+}
+
