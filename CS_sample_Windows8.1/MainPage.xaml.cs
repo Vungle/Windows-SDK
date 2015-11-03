e29139298e8b6a81a97c8ac567f66afc4e79db4a
vadimZasovin <vzasovin@gmail.com>
2015-11-03 19:17:04 +0300

    added windows 8.1 CS sample app

---
+++ CS_sample_Windows8.1/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using VungleSDK;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
+
+namespace CS_sample_Windows8._1
+{
+    public sealed partial class MainPage : Page
+    {
+
+        VungleAd sdkInstance;
+        bool adPlayable;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            //Obtain Vungle SDK instance
+            sdkInstance = AdFactory.GetInstance("vungleTest");
+
+            //Register OnAdPlayableChanged event handler
+            sdkInstance.OnAdPlayableChanged += SdkInstance_OnAdPlayableChanged;
+        }
+
+        //Event handler for OnAdPlayableChanged event
+        private async void SdkInstance_OnAdPlayableChanged(object sender, AdPlayableEventArgs e)
+        {
+            adPlayable = e.AdPlayable;
+            //Run asynchronously on the UI thread
+            await CoreApplication.MainView.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                new DispatchedHandler(() => ChangeButtonsState()));
+        }
+
+        private void ChangeButtonsState()
+        {
+            //Change IsEnabled property for each button
+            DefaultConfigButton.IsEnabled = adPlayable;
+            IncentivizedConfigButton.IsEnabled = adPlayable;
+            MutedConfigButton.IsEnabled = adPlayable;
+        }
+
+        private async void DefaultConfigButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Play ad with default configuration
+            await sdkInstance.PlayAdAsync(new AdConfig());
+        }
+
+        private async void IncentivizedConfigButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Play ad with enabled 'incentivized' option
+            await sdkInstance.PlayAdAsync(new AdConfig { Incentivized = true });
+        }
+
+        private async void MutedConfigButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Play ad without sound
+            await sdkInstance.PlayAdAsync(new AdConfig { SoundEnabled = false });
+        }
+    }
+}
